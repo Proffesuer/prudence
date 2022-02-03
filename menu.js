d5f5638b7537a12832850aa535f557a7a0c25bd4
Proffesuer <levischisira0@gmail.com>
2022-02-03 17:54:33 +0300

    adding my scripting file for the menu

---
+++ menu.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function openNav() {
+  document.getElementById("myBars").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myBars").style.width = "0%";
+}
